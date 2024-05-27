7db6f14025ad500de37c8b4f6120c31f4c3f26ed
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-05-27 18:01:48 -0300

    Atualizar o Account.java
    adição de script

--- BancoDigital/src/bank/account/Account.java
+++ BancoDigital/src/bank/account/Account.java
@@ -1,1 +1,27 @@
+package bank.account;
 
+public abstract class Account {
+    protected double saldo;
+
+    public Conta(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double quantia) {
+        saldo += quantia;
+    }
+
+    public void sacar(double quantia) {
+        if(quantia <= saldo) {
+            saldo -= quantia;
+        } else {
+            System.out.println("Fundos insuficientes");
+        }
+    }
+
+    public abstract void imprimirExtrato();
+}
