99bcb4d6170192f8a4a2dcf4fa6208536509f894
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-05-27 23:25:01 -0300

    Atualizar o Client.java
    adição de script

--- BancoDigital/src/bank/client/Client.java
+++ BancoDigital/src/bank/client/Client.java
@@ -1,1 +1,27 @@
+package bank.client;
+
+import java.util.ArrayList;
+import java.util.List;
+import bank.account.Account;
+
+public class Client {
+    private String name;
+    private List<Account> accounts;
+
+    public Client(String name) {
+        this.name = name;
+        this.accounts = new ArrayList<>();
+    }
+
+    public void adicionarConta(Account account) {
+        accounts.add(account);
+    }
+
+    public void imprimirContas() {
+        System.out.println("Cliente: " + name);
+        for (Account account : accounts) {
+            account.imprimirExtrato();
+        }
+    }
+}
 
