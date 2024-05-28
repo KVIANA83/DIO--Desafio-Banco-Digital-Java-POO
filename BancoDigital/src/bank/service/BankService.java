86b7591b358cd09de1f5cb71f14f6528364a41e1
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-05-27 23:28:14 -0300

    Atualizar o BankService.java
    adição de script

--- BancoDigital/src/bank/service/BankService.java
+++ BancoDigital/src/bank/service/BankService.java
@@ -1,1 +1,25 @@
+package bank.service;
+
+import bank.account.Conta;
+import bank.account.ContaCorrente;
+import bank.account.ContaPoupanca;
+import bank.client.Client;
+
+public class BankService {
+
+    public static void main(String[] args) {
+        Client client = new Client("Karine Viana");
+        
+        Conta contaCorrente = new ContaCorrente(1000);
+        Conta contaPoupanca = new ContaPoupanca(2000);
+        
+        client.adicionarConta(contaCorrente);
+        client.adicionarConta(contaPoupanca);
+
+        contaCorrente.depositar(500);
+        contaPoupanca.sacar(300);
+
+        client.imprimirContas();
+    }
+}
 
