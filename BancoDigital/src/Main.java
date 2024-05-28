3e282d652d9a5f4b47de7efa223e75965359324c
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-05-27 23:19:03 -0300

    Atualizar o Main.java
    adição de script

--- BancoDigital/src/Main.java
+++ BancoDigital/src/Main.java
@@ -1,7 +1,24 @@
+package bank;
+
+import bank.account.Account;
+import bank.account.CheckingAccount;
+import bank.account.SavingsAccount;
+import bank.client.Client;
+
 public class Main {
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Client client = new Client("John Doe");
+        
+        Account checkingAccount = new CheckingAccount(1000);
+        Account savingsAccount = new SavingsAccount(2000);
+        
+        client.addAccount(checkingAccount);
+        client.addAccount(savingsAccount);
+
+        checkingAccount.deposit(500);
+        savingsAccount.withdraw(300);
+
+        client.printAccounts();
     }
-    
 }
